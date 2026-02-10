fe1f16b63af6227c97d8dadf749184add225cfdb
Exp10sive <arslan.seytmemetov@mail.ru>
2026-02-10 09:29:32 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); 
+
+    char ch;
+
+    while (cin >> ch) {
+        int code = ch;
+
+        cout << ch << " → код " << code << " → ";
+  
+
+        if ((code >= 65 && code <= 70) ||
+            (code >= 48 && code <= 57)) {
+            cout << "Шестнадцатеричная цифра (корректная)\n";
+        }
+        else {
+            cout << "НЕ является hex-цифрой\n";
+        }
+    }
+
+    cout << "\nКонец ввода.\n";
+    return 0;
+}
+
